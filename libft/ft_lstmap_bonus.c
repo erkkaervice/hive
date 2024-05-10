1828a2d1b9d6d931cf9cfcde59f3ca8f9ecdf26b
erkkaervice <erkka@ervice.fi>
2024-05-10 14:55:30 +0300

    updated my lstmap

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,50 +6,11 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 16:39:43 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/05/09 18:05:40 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/05/10 12:01:19 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static t_list	*lst_new(void *content)
-{
-	t_list	*new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	return (new);
-}
-
-static void	lst_clear(t_list **lst, void (*del)(void *))
-{
-	if (!lst || !del || !(*lst))
-		return ;
-	lst_clear(&(*lst)->next, del);
-	(del)((*lst)->content);
-	free(*lst);
-	*lst = NULL;
-}
-
-static void	lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*ptr;
-
-	if (!lst || !new)
-		return ;
-	if (!(*lst))
-	{
-		*lst = new;
-		return ;
-	}
-	ptr = *lst;
-	while (ptr->next)
-		ptr = ptr->next;
-	ptr->next = new;
-}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -57,20 +18,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_node;
 	void	*set;
 
+	new_list = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
-	new_list = NULL;
 	while (lst)
 	{
 		set = f(lst->content);
-		new_node = lst_new(set);
+		new_node = ft_lstnew(set);
 		if (!new_node)
 		{
 			del(set);
-			lst_clear(&new_list, (*del));
+			ft_lstclear(&new_list, (*del));
 			return (new_list);
 		}
-		lstadd_back(&new_list, new_node);
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
 	return (new_list);
