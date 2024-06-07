fa03ad662414c2ee196cd7cc2808b7a8253039d7
erkkaervice <erkka@ervice.fi>
2024-06-07 15:34:21 +0300

    gnl_testers

--- area51/test_next_line.c
+++ area51/test_next_line.c
@@ -1,88 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_next_line.c                                   :+:      :+:    :+:   */
+/*   test_main_line.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/28 16:02:39 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/06/06 12:35:48 by eala-lah         ###   ########.fr       */
+/*   Created: 2024/06/07 15:15:51 by eala-lah          #+#    #+#             */
+/*   Updated: 2024/06/07 15:15:59 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "get_next_line.h"
 
-int	open_file(const char *file)
+int	main(void)
 {
-	int	fd;
-
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Error opening file");
-		exit(EXIT_FAILURE);
-	}
-	return (fd);
-}
-
-void	open_files_recursively(int *fd, char **files, int index, int count)
-{
-	if (index < count)
-	{
-		fd[index] = open_file(files[index]);
-		open_files_recursively(fd, files, index + 1, count);
-	}
-}
-
-void	read_files_recursively(int *fd, int index, int file_count, int *active)
-{
-	char	*line;
-
-	if (*active == 0)
-		return ;
-
-	if (fd[index] != -1)
-	{
-		line = get_next_line(fd[index]);
-		if (line)
-		{
-			printf("File %d: %s\n", index + 1, line);
-			free(line);
-		}
-		else
-		{
-			close(fd[index]);
-			fd[index] = -1;
-			(*active)--;
-		}
-	}
-	index = (index + 1) % file_count;
-	read_files_recursively(fd, index, file_count, active);
-}
-
-int	main(int argc, char **argv)
-{
-	int	*fd;
-	int	active_files;
-
-	if (argc < 2)
-	{
-		fprintf(stderr, "Usage: %s <filename1> <filename2> ...\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-	fd = (int *)malloc(sizeof(int) * (argc - 1));
-	if (!fd)
-	{
-		perror("Error allocating memory");
-		return (EXIT_FAILURE);
-	}
-	open_files_recursively(fd, argv + 1, 0, argc - 1);
-	active_files = argc - 1;
-	read_files_recursively(fd, 0, argc - 1, &active_files);
-	free(fd);
-	return (EXIT_SUCCESS);
+	printf("%s", get_next_line(0));
 }
---
+++ area51/test_next_line_bonus.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_next_line.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 16:02:39 by eala-lah          #+#    #+#             */
+/*   Updated: 2024/06/07 15:29:01 by eala-lah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	open_file(const char *f)
+{
+	int	fd;
+
+	fd = open(f, O_RDONLY);
+	return (fd);
+}
+
+void	open_files_recursively(int *fd, char **fs, int i, int pax)
+{
+	if (i < pax)
+	{
+		fd[i] = open_file(fs[i]);
+		open_files_recursively(fd, fs, i + 1, pax);
+	}
+}
+
+void	read_files_recursively(int *fd, int i, int fc, int *on)
+{
+	char	*line;
+
+	if (*on == 0)
+		return ;
+
+	if (fd[i] != -1)
+	{
+		line = get_next_line(fd[i]);
+		if (line)
+		{
+			printf("File %d: %s\n", i + 1, line);
+			free(line);
+		}
+		else
+		{
+			close(fd[i]);
+			fd[i] = -1;
+			(*on)--;
+		}
+	}
+	i = (i + 1) % fc;
+	read_files_recursively(fd, i, fc, on);
+}
+
+int	main(int argc, char **argv)
+{
+	int	*fd;
+	int	af;
+
+	fd = (int *)malloc(sizeof(int) * (argc - 1));
+	open_files_recursively(fd, argv + 1, 0, argc - 1);
+	af = argc - 1;
+	read_files_recursively(fd, 0, argc - 1, &af);
+	free(fd);
+	return (EXIT_SUCCESS);
+}
