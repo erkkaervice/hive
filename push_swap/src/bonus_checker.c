c601e149258919d3653b9d53643db99bb358f406
eala-lah <eala-lah@student.hive.fi>
2024-07-25 17:57:49 +0300

    final cleanup.

--- push_swap/src/bonus_checker.c
+++ push_swap/src/bonus_checker.c
@@ -6,7 +6,7 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 13:29:55 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/07/25 17:39:16 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/07/25 17:52:11 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,27 @@ t_stack	*ft_stack(int ac, char **av)
 int	ft_compare(char *line, t_stack **sta, t_stack **stb)
 {
 	if (ft_strncmp(line, "sa\n", 3) == 0)
-		ft_sa((t_stack **)sta);
+		ft_sa(sta);
 	else if (ft_strncmp(line, "sb\n", 3) == 0)
-		ft_sb((t_stack **)stb);
+		ft_sb(stb);
 	else if (ft_strncmp(line, "ss\n", 3) == 0)
-		ft_ss((t_stack **)sta, (t_stack **)stb);
+		ft_ss(sta, stb);
 	else if (ft_strncmp(line, "pa\n", 3) == 0)
-		ft_pa((t_stack **)sta, (t_stack **)stb);
+		ft_pa(sta, stb);
 	else if (ft_strncmp(line, "pb\n", 3) == 0)
-		ft_pb((t_stack **)stb, (t_stack **)sta);
+		ft_pb(stb, sta);
 	else if (ft_strncmp(line, "ra\n", 3) == 0)
-		ft_ra((t_stack **)sta);
+		ft_ra(sta);
 	else if (ft_strncmp(line, "rb\n", 3) == 0)
-		ft_rb((t_stack **)stb);
+		ft_rb(stb);
 	else if (ft_strncmp(line, "rr\n", 3) == 0)
-		ft_rr((t_stack **)sta, (t_stack **)stb);
+		ft_rr(sta, stb);
 	else if (ft_strncmp(line, "rra\n", 4) == 0)
-		ft_rra((t_stack **)sta);
+		ft_rra(sta);
 	else if (ft_strncmp(line, "rrb\n", 4) == 0)
-		ft_rrb((t_stack **)stb);
+		ft_rrb(stb);
 	else if (ft_strncmp(line, "rrr\n", 4) == 0)
-		ft_rrr((t_stack **)sta, (t_stack **)stb);
+		ft_rrr(sta, stb);
 	else
 		return (write(2, "Error\n", 6), 1);
 	return (0);
@@ -90,13 +90,8 @@ int	ft_cleaner(char **line, t_stack **sta, t_stack **stb)
 
 void	ft_result(t_stack **sta, t_stack **stb)
 {
-	if (!ft_sorted(*sta))
-	{
-		if (!(*stb))
-			write(1, "OK\n", 3);
-		else
-			write(1, "KO\n", 3);
-	}
+	if (!ft_sorted(*sta) && !(*stb))
+		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
 }
@@ -111,10 +106,7 @@ int	main(int ac, char **av)
 		return (0);
 	sta = ft_stack(ac, av);
 	if (!sta)
-	{
-		write(2, "Error\n", 6);
-		return (0);
-	}
+		return (write(2, "Error\n", 6), 0);
 	stb = NULL;
 	line = get_next_line(0);
 	while (line != NULL)
