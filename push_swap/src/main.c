c7f048a6036136713315aa51df6a74950ea253a7
eala-lah <eala-lah@student.hive.fi>
2024-07-30 16:56:38 +0300

    reorganising

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,11 +6,52 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:41:12 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/07/30 16:33:36 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/07/30 16:55:21 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_stepcount(t_stack **sta, t_stack **stb)
+{
+	t_stack	*tmp;
+
+	tmp = *stb;
+	while (tmp)
+	{
+		if (tmp->pos > ft_stalen(*stb) / 2)
+			tmp->sob = (ft_stalen(*stb) - tmp->pos) * -1;
+		else
+			tmp->sob = tmp->pos;
+		if (tmp->tgt > ft_stalen(*sta) / 2)
+			tmp->soa = (ft_stalen(*sta) - tmp->tgt) * -1;
+		else
+			tmp->soa = tmp->tgt;
+		tmp = tmp->next;
+	}
+}
+
+void	ft_quickstep(t_stack **sta, t_stack **stb)
+{
+	t_stack	*tmp;
+	int		qs;
+	int		soa;
+	int		sob;
+
+	tmp = *stb;
+	qs = INT_MAX;
+	while (tmp)
+	{
+		if (ft_abs(tmp->soa) + ft_abs(tmp->sob) < ft_abs(qs))
+		{
+			qs = ft_abs(tmp->sob) + ft_abs(tmp->soa);
+			soa = tmp->soa;
+			sob = tmp->sob;
+		}
+		tmp = tmp->next;
+	}
+	ft_move(sta, stb, soa, sob);
+}
 
 static void	push_swap(t_stack **sta, t_stack **stb, int sts)
 {
