70566e63da1ea85cb2055acbf3ce4ed8dee53f73
eala-lah <eala-lah@student.hive.fi>
2024-08-20 15:57:15 +0300

    header libraries creation

--- minitalk/inc/bonus_minitalk.h
+++ minitalk/inc/bonus_minitalk.h
@@ -6,7 +6,14 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:08 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/08/20 15:49:23 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/08/20 15:55:21 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BONUS_MINITALK_H
+# define BONUS_MINITALK.H
+
+# include "../libft/inc/libft.h"
+# include <signal.h>
+
+#endif
--- minitalk/inc/minitalk.h
+++ minitalk/inc/minitalk.h
@@ -6,7 +6,14 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:05 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/08/20 15:49:25 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/08/20 15:54:03 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libft/inc/libft.h"
+# include <signal.h>
+
+#endif
