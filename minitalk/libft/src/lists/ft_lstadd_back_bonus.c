e9159ef52da495a622b07f04a550d2aa41fd7af6
eala-lah <eala-lah@student.hive.fi>
2024-08-26 11:40:41 +0300

    added libft to minitalk

---
+++ minitalk/libft/src/lists/ft_lstadd_back_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 16:39:03 by eala-lah          #+#    #+#             */
+/*   Updated: 2024/05/01 16:40:27 by eala-lah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
