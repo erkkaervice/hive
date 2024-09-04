136e190994018ad3d244c0fa373015f683f6622a
eala-lah <eala-lah@student.hive.fi>
2024-09-04 15:04:34 +0300

    norminetted

--- minitalk/inc/minitalk.h
+++ minitalk/inc/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:05 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/04 14:16:50 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:04:25 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include "../libft/inc/libft.h"
 # include <signal.h>
 
-typedef struct signal {
+typedef struct signal
+{
 	int	bit;
 	int	chr;
 }	t_signal;
