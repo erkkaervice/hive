6a080cb1091d07a6aba8bac3afc61fc76921912a
eala-lah <eala-lah@student.hive.fi>
2024-09-04 16:54:41 +0300

    fixed the ack signal

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:33 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/04 16:34:12 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/04 16:54:28 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 volatile sig_atomic_t	g_ack;
 
-void	signal_handler(int sig)
+void	ft_acker(int sig)
 {
 	if (sig == SIGUSR1)
 		g_ack = 1;
 }
 
-void	send_bit(int server_pid, int bit)
+void	ft_confirm(int end, int bit)
+{
+	if (end && bit == 0)
+		ft_printf("VERY SUCCESS!\n");
+}
+
+void	ft_bits(int server_pid, int bit)
 {
 	if (bit)
 	{
@@ -37,16 +43,17 @@ void	send_bit(int server_pid, int bit)
 	g_ack = 0;
 }
 
-void	send_character(int server_pid, char c)
+void	ft_send(int server_pid, char c, int end)
 {
-	send_bit(server_pid, (c & 0x80) != 0);
-	send_bit(server_pid, (c & 0x40) != 0);
-	send_bit(server_pid, (c & 0x20) != 0);
-	send_bit(server_pid, (c & 0x10) != 0);
-	send_bit(server_pid, (c & 0x08) != 0);
-	send_bit(server_pid, (c & 0x04) != 0);
-	send_bit(server_pid, (c & 0x02) != 0);
-	send_bit(server_pid, (c & 0x01) != 0);
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		ft_bits(server_pid, (c & (1 << bit)) != 0);
+		ft_confirm(end, bit);
+		bit--;
+	}
 }
 
 int	main(int argc, char **argv)
@@ -56,21 +63,21 @@ int	main(int argc, char **argv)
 	struct sigaction	sa;
 
 	if (argc != 3 || !(*argv[2]))
-		ft_error("USAGE: ./client_bonus <server_pid> <message>");
+		ft_error("USAGE: ./client <server_pid> <message>");
 	server_pid = ft_atoi(argv[1]);
 	if (server_pid <= 0)
 		ft_error("INVALID SERVER PID");
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	sa.sa_handler = signal_handler;
+	sa.sa_handler = ft_acker;
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
 	msg = argv[2];
 	while (*msg)
 	{
-		send_character(server_pid, *msg);
+		ft_send(server_pid, *msg, 0);
 		msg++;
 	}
-	send_character(server_pid, '\0');
+	ft_send(server_pid, '\0', 1);
 	return (0);
 }
