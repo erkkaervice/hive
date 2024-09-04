7e35984b4122fd426670a9cc7a055c5ee8985c76
eala-lah <eala-lah@student.hive.fi>
2024-09-04 13:15:32 +0300

    server finally works without global struct variable

--- minitalk/src/client_bonus.c
+++ minitalk/src/client_bonus.c
@@ -6,72 +6,100 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:43 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/03 17:26:52 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:13:08 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
-volatile sig_atomic_t	g_response;
+volatile sig_atomic_t g_response;
 
-void	feedback_handler(int sig)
+void feedback_handler(int sig)
 {
-	if (sig == SIGUSR1)
-		g_response = 1;
+    if (sig == SIGUSR1)
+    {
+        g_response = 1;
+    }
 }
 
-void	wait_for_feedback(int is_end, int bit)
+void wait_for_feedback(int is_end, int bit)
 {
-	if (g_response == 0)
-		pause();
-	if (is_end && (bit == 0))
-		ft_printf("Received\n");
-	g_response = 0;
+    while (g_response == 0)
+    {
+        pause();  // Wait for acknowledgment
+    }
+    if (is_end && (bit == 0))
+    {
+        ft_printf("Received\n");
+    }
+    g_response = 0;  // Reset response flag
 }
 
-void	ctob(int server_pid, char c, int is_end)
+void ctob(int server_pid, char c, int is_end)
 {
-	int					bit;
-	struct sigaction	sa;
+    int bit;
+    struct sigaction sa;
 
-	bit = 7;
-	g_response = 0;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = feedback_handler;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
-	while (bit >= 0)
-	{
-		if ((c & (0x1 << bit)) && (kill (server_pid, SIGUSR1) == -1))
-			ft_error("ERROR IN SENDING SIGNAL");
-		if (!(c & (0x1 << bit)) && (kill (server_pid, SIGUSR2) == -1))
-			ft_error("ERROR IN SENDING SIGNAL");
-		wait_for_feedback(is_end, bit);
-		bit --;
-	}
+    bit = 7;
+    g_response = 0;
+
+    // Set up signal handler for feedback
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = feedback_handler;
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
+    }
+
+    // Send each bit of the character
+    while (bit >= 0)
+    {
+        if ((c & (1 << bit)) != 0)
+        {
+            if (kill(server_pid, SIGUSR1) == -1)
+            {
+                ft_error("ERROR IN SENDING SIGNAL");
+            }
+        }
+        else
+        {
+            if (kill(server_pid, SIGUSR2) == -1)
+            {
+                ft_error("ERROR IN SENDING SIGNAL");
+            }
+        }
+        wait_for_feedback(is_end, bit);  // Wait for server acknowledgment
+        bit--;
+    }
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int					server_pid;
-	char				*msg;
+    int server_pid;
+    char *msg;
 
-	msg = NULL;
-	if (argc != 3 || (!(*argv[2])))
-		ft_error("ERROR IN INPUTS");
-	else
-	{
-		server_pid = ft_atoi(argv[1]);
-		if (server_pid <= 0)
-			ft_error("ERROR IN INPUTS");
-		msg = argv[2];
-	}
-	while (*msg)
-	{
-		ctob(server_pid, *msg, 0);
-		msg ++;
-	}
-	ctob(server_pid, *msg, 1);
-	return (0);
+    if (argc != 3 || !(*argv[2]))
+    {
+        ft_error("ERROR IN INPUTS");
+    }
+    else
+    {
+        server_pid = ft_atoi(argv[1]);
+        if (server_pid <= 0)
+        {
+            ft_error("ERROR IN INPUTS");
+        }
+        msg = argv[2];
+    }
+
+    while (*msg)
+    {
+        ctob(server_pid, *msg, 0);  // Send each character
+        msg++;
+    }
+    ctob(server_pid, *msg, 1);  // Send end-of-message signal
+
+    return 0;
 }
--- minitalk/src/server_bonus.c
+++ minitalk/src/server_bonus.c
@@ -6,50 +6,64 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:53 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/04 12:04:10 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:14:47 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
-t_signal	g_signal;
-
-static void	btoc(int sig, siginfo_t *info, void *other)
+void btoc(int sig, siginfo_t *info, void *context)
 {
-	pid_t		client_pid;
+    static int current_byte = 0;
+    static int bit = 7;
 
-	(void) other;
-	client_pid = info->si_pid;
-	if (sig == SIGUSR1)
-		g_signal.current_byte = g_signal.current_byte | (1 << g_signal.bit);
-	g_signal.bit --;
-	if (g_signal.bit == -1)
-	{
-		if (g_signal.current_byte == '\0')
-			ft_printf("\n");
-		else
-			write(1, &(g_signal.current_byte), 1);
-		g_signal.current_byte = 0;
-		g_signal.bit = 7;
-	}
-	if (kill (client_pid, SIGUSR1) == -1)
-		ft_error("ERROR IN SENDING SIGNAL");
+    (void)context;  // Unused parameter
+    if (sig == SIGUSR1)
+    {
+        current_byte |= (1 << bit);
+    }
+    else if (sig == SIGUSR2)
+    {
+        current_byte &= ~(1 << bit);
+    }
+    bit--;
+    if (bit == -1)
+    {
+        if (current_byte == '\0')
+        {
+            ft_printf("\n");  // End of message
+        }
+        else
+        {
+            write(1, &current_byte, 1);  // Print received byte
+        }
+        current_byte = 0;
+        bit = 7;  // Reset for next byte
+    }
+    if (kill(info->si_pid, SIGUSR1) == -1)
+        ft_error("ERROR IN SENDING SIGNAL");
 }
 
-int	main(void)
+int main(void)
 {
-	struct sigaction	sa;
+    struct sigaction sa;
 
-	g_signal.current_byte = 0;
-	g_signal.bit = 7;
-	ft_printf("Server PID: %d\n", getpid());
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = btoc;
-	if ((sigaction(SIGUSR1, &sa, NULL) == -1)
-		|| (sigaction(SIGUSR2, &sa, NULL) == -1))
-		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
-	while (1)
-		pause();
-	return (0);
+    ft_printf("Server PID: %d\n", getpid());
+
+    // Initialize sigaction for handling signals
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = btoc;
+
+    if ((sigaction(SIGUSR1, &sa, NULL) == -1) || (sigaction(SIGUSR2, &sa, NULL) == -1))
+    {
+        ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
+    }
+
+    while (1)
+    {
+        pause();  // Wait for signals indefinitely
+    }
+
+    return 0;
 }
