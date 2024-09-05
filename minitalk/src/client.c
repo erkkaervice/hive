d822b6825a3d8c260cc5115b97fe8e150b589df2
eala-lah <eala-lah@student.hive.fi>
2024-09-05 16:18:20 +0300

    finalized versions.

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,21 +6,21 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:33 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/05 15:40:12 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:17:21 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-volatile sig_atomic_t g_ack;
+volatile sig_atomic_t	g_ack;
 
-void	ft_ack_handler(int sig)
+void	ft_acker(int sig)
 {
 	if (sig == SIGUSR1)
 		g_ack = 1;
 }
 
-void	ft_send_bit(int server_pid, int bit)
+void	ft_bits(int server_pid, int bit)
 {
 	if (bit)
 	{
@@ -37,25 +37,23 @@ void	ft_send_bit(int server_pid, int bit)
 	g_ack = 0;
 }
 
-void	ft_send_char(int server_pid, char c, int end)
+void	ft_send(int server_pid, char c)
 {
-	int bit;
+	int	bit;
 
 	bit = 7;
 	while (bit >= 0)
 	{
-		ft_send_bit(server_pid, (c >> bit) & 1);
+		ft_bits(server_pid, (c >> bit) & 1);
 		bit--;
 	}
-	if (end && !c)
-		ft_printf("VERY SUCCESS!\n");
 }
 
 int	main(int argc, char **argv)
 {
-	pid_t server_pid;
-	char *msg;
-	struct sigaction sa;
+	pid_t				server_pid;
+	char				*msg;
+	struct sigaction	sa;
 
 	if (argc != 3 || !(*argv[2]))
 		ft_error("USAGE: ./client <server_pid> <message>");
@@ -64,14 +62,12 @@ int	main(int argc, char **argv)
 		ft_error("INVALID SERVER PID");
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	sa.sa_handler = ft_ack_handler;
+	sa.sa_handler = ft_acker;
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
 	msg = argv[2];
 	while (*msg)
-	{
-		ft_send_char(server_pid, *msg++, 0);
-	}
-	ft_send_char(server_pid, '\0', 1);
+		ft_send(server_pid, *msg++);
+	ft_send(server_pid, '\0');
 	return (0);
 }
--- minitalk/src/client_bonus.c
+++ minitalk/src/client_bonus.c
@@ -6,74 +6,70 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:43 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/04 15:37:55 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:18:06 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
-volatile sig_atomic_t	g_res;
+volatile sig_atomic_t	g_ack;
 
-void	ft_handler(int sig)
+void	ft_acker(int sig)
 {
 	if (sig == SIGUSR1)
-		g_res = 1;
+		g_ack = 1;
 }
 
-void	ft_confirm(int end, int bit)
+void	ft_bits(int server_pid, int bit)
 {
-	while (g_res == 0)
+	if (bit)
+	{
+		if (kill(server_pid, SIGUSR1) == -1)
+			ft_error("ERROR IN SENDING SIGUSR1");
+	}
+	else
+	{
+		if (kill(server_pid, SIGUSR2) == -1)
+			ft_error("ERROR IN SENDING SIGUSR2");
+	}
+	while (!g_ack)
 		pause();
-	if (end && (bit == 0))
-		ft_printf("Received\n");
-	g_res = 0;
+	g_ack = 0;
 }
 
 void	ft_send(int server_pid, char c, int end)
 {
-	int					bit;
-	struct sigaction	sa;
+	int	bit;
 
 	bit = 7;
-	g_res = 0;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = ft_handler;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
 	while (bit >= 0)
 	{
-		if ((c & (1 << bit)) != 0)
-		{
-			if (kill(server_pid, SIGUSR1) == -1)
-				ft_error("ERROR IN SENDING SIGNAL");
-		}
-		else
-		{
-			if (kill(server_pid, SIGUSR2) == -1)
-				ft_error("ERROR IN SENDING SIGNAL");
-		}
-		ft_confirm(end, bit);
+		ft_bits(server_pid, (c >> bit) & 1);
 		bit--;
 	}
+	if (end && !c)
+		ft_printf("VERY SUCCESS!\n");
 }
 
 int	main(int argc, char **argv)
 {
-	int		server_pid;
-	char	*msg;
+	pid_t				server_pid;
+	char				*msg;
+	struct sigaction	sa;
 
 	if (argc != 3 || !(*argv[2]))
-		ft_error("ERROR IN INPUTS");
+		ft_error("USAGE: ./client <server_pid> <message>");
 	server_pid = ft_atoi(argv[1]);
 	if (server_pid <= 0)
-		ft_error("ERROR IN INPUTS");
+		ft_error("INVALID SERVER PID");
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = ft_acker;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
 	msg = argv[2];
 	while (*msg)
-	{
-		ft_send(server_pid, *msg, 0);
-		msg++;
-	}
-	ft_send(server_pid, *msg, 1);
+		ft_send(server_pid, *msg++, 0);
+	ft_send(server_pid, '\0', 1);
 	return (0);
 }
