a3dbf8a28ac01f16dc38a17a9d71ffbe8433ed98
eala-lah <eala-lah@student.hive.fi>
2024-09-09 14:21:32 +0300

    No repeated long message crashes the server when interrupted.

--- minitalk/src/server_bonus.c
+++ minitalk/src/server_bonus.c
@@ -6,55 +6,60 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:53 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/09 12:36:16 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:20:53 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
-volatile sig_atomic_t	g_oke = false;
+volatile sig_atomic_t g_state = 0; // 0: g_oke=false, 1: g_oke=true
 
-int	ft_verify(siginfo_t *info, pid_t *current_pid)
-{
-	if (g_oke == true && info->si_pid != *current_pid) 
-		return (-1);
-	else if (g_oke == false)
-	{
-		*current_pid = info->si_pid;
-		g_oke = true;
-	}
-	return (0);
-}
-
-void	ft_recieve(int sig, siginfo_t *info, void *birds)
+void	ft_receive(int sig, siginfo_t *info, void *birds)
 {
 	static int	chr = 0;
 	static int	bit = 7;
-	static pid_t	current_pid = 0;
+	static pid_t last_pid = 0;
 
 	(void)birds;
-	if (ft_verify(info, &current_pid) == -1)
-		return ;
+
+	if (g_state == 0) // Not currently processing a message
+	{
+		last_pid = info->si_pid; // Set the current PID
+		g_state = 1; // Indicate that we are now processing a message
+	}
+	else if (info->si_pid != last_pid)
+	{
+		// If the PID does not match, ignore the signal
+		return;
+	}
+
 	if (sig == SIGUSR1)
-		chr |= (1 << bit);
-	else
-		chr &= ~(1 << bit);
+		chr |= (1 << bit); // Set the bit for SIGUSR1
+	else if (sig == SIGUSR2)
+		chr &= ~(1 << bit); // Clear the bit for SIGUSR2
+
 	bit--;
 	if (bit < 0)
 	{
+		// We have processed a full byte
 		if (chr == '\0')
 		{
+			// End of message
 			write(1, "\n", 1);
-			if (kill(info->si_pid, SIGUSR1) == -1)
+			if (kill(last_pid, SIGUSR1) == -1)
 				ft_error("ERROR IN SENDING SIGNAL");
-			g_oke = false;
+			g_state = 0; // Reset state for next message
 		}
 		else
+		{
+			// Print received character
 			write(1, &chr, 1);
+		}
+		// Reset for the next byte
 		chr = 0;
 		bit = 7;
 	}
-	if (kill(info->si_pid, SIGUSR1) == -1)
+	if (kill(last_pid, SIGUSR1) == -1)
 		ft_error("ERROR IN SENDING SIGNAL");
 }
 
@@ -65,11 +70,11 @@ int	main(void)
 	ft_printf("Server PID: %d\n", getpid());
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = ft_recieve;
+	sa.sa_sigaction = ft_receive;
 	if ((sigaction(SIGUSR1, &sa, NULL) == -1)
 		|| (sigaction(SIGUSR2, &sa, NULL) == -1))
 		ft_error("ERROR IN SETTING UP SIGNAL HANDLER");
 	while (1)
-		pause();
+		pause(); // Wait for signals
 	return (0);
 }
