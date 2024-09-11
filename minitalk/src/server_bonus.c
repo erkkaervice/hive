05583dceb3e122189e61d8e57c9c625161fe9d5c
eala-lah <eala-lah@student.hive.fi>
2024-09-11 12:00:13 +0300

    working version of a buffer mechanism to only print a full message. Needs to be divided into 2 separate functions.

--- minitalk/src/server_bonus.c
+++ minitalk/src/server_bonus.c
@@ -6,11 +6,12 @@
 /*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:49:53 by eala-lah          #+#    #+#             */
-/*   Updated: 2024/09/11 11:45:35 by eala-lah         ###   ########.fr       */
+/*   Updated: 2024/09/11 11:57:43 by eala-lah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
+#include <stdlib.h>
 
 volatile sig_atomic_t	g_state = 0;
 
@@ -25,15 +26,56 @@ void	ft_bit(int sig, int *chr, int *bit)
 
 void	ft_eom(int chr, pid_t last_pid)
 {
-	if (chr == '\0')
+	static char	*buffer = NULL;
+	static int	index = 0;
+	static int	buffer_size = 128;  // Initial buffer size
+	int			i;
+
+	// Allocate the initial buffer if not already allocated
+	if (!buffer)
 	{
-		write(1, "\n", 1);
+		buffer = malloc(buffer_size);
+		if (!buffer)
+			ft_error("MEMORY ALLOCATION FAILED\n");
+	}
+
+	if (chr == '\0')  // End of message
+	{
+		buffer[index] = '\0';  // Null-terminate the string
+		write(1, buffer, index);  // Print the entire message
+		write(1, "\n", 1);  // Newline after message
+		free(buffer);  // Free the allocated memory
+		buffer = NULL;  // Reset buffer for the next message
+		index = 0;
+		buffer_size = 128;  // Reset buffer size
 		if (kill(last_pid, SIGUSR1) == -1)
 			ft_error("PROBLEM WITH SIGNAL, TRY TELEGRAM\n");
-		g_state = 0;
+		g_state = 0;  // Reset state for new client
 	}
 	else
-		write(1, &chr, 1);
+	{
+		if (index >= buffer_size)
+		{
+			// Allocate a larger buffer (double the size)
+			char *new_buffer = malloc(buffer_size * 2);
+			if (!new_buffer)
+				ft_error("MEMORY ALLOCATION FAILED\n");
+
+			// Copy contents from the old buffer to the new one
+			i = 0;
+			while (i < index)
+			{
+				new_buffer[i] = buffer[i];
+				i++;
+			}
+
+			// Free the old buffer and point to the new one
+			free(buffer);
+			buffer = new_buffer;
+			buffer_size *= 2;  // Double the buffer size
+		}
+		buffer[index++] = chr;  // Store received character in buffer
+	}
 }
 
 void	ft_receive(int sig, siginfo_t *info, void *birds)
