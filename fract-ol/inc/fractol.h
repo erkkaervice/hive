636987e13fdd4b28658b8a6ab3d4e237e0c6e636
eala-lah <eala-lah@student.hive.fi>
2024-10-03 15:57:18 +0300

    started main and libraries

---
+++ fract-ol/inc/fractol.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eala-lah <eala-lah@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 15:49:18 by eala-lah          #+#    #+#             */
+/*   Updated: 2024/10/03 15:55:32 by eala-lah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../minilibx-linux/mlx.h"
+# include "../libft/inc/libft.h"
+# include "keys.h"
+
+# define WIDTH 900
+# define HEIGHT 900
+
+# define MANDELBROT 1
+# define JULIA 2
+
+#endif
